585885343eb8a15326d53f0a45784e9b34400357
Yuliya Astapova <yva2@illinois.edu>
2019-04-18 11:11:59 -0400

    Added api code

--- script.py
+++ script.py
@@ -1,9 +1,47 @@
-from flask import Flask
-from serve import useless_function  
+import json
+from flask import Flask, request
+import serve
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-# Define our "ping" end point
-@app.route('/ping')
-def useless_output():
-  return(useless_function())
+# dummy ping page
+@app.route('/', methods=['GET'])
+@app.route('/ping', methods=['GET'])
+def index():
+    return "Simple TextBlob API is running."
+
+# tag input text with part-of-speech tags
+@app.route('/TagPOS', methods=['POST'])
+def tag_pos():
+    text = request.json
+    pos = serve.tag_pos(text)
+    response = json.dumps(pos)
+
+    return response
+
+# calculate sentiment polarity score of input text [-1, 1]
+@app.route('/CalcSentimentScore', methods=['POST'])
+def calc_sentiment_score():
+    text = request.json
+    sent = serve.calc_sentiment_score(text)
+    response = json.dumps(sent)
+
+    return response
+
+# autocorrect any spelling mistakes in input text
+@app.route('/AutocorrectText', methods=['POST'])
+def autocorrect_text():
+    text = request.json
+    new_text = serve.autocorrect_text(text)
+    response = json.dumps(new_text)
+
+    return response
+
+# translate input text to the given language
+@app.route('/TranslateText/<to_lang>', methods=['POST'])
+def translate_text(to_lang):
+    text = request.json
+    translated = serve.translate_text(text, to_lang)
+    response = json.dumps(translated)
+
+    return response
--- serve.py
+++ serve.py
@@ -1,5 +1,29 @@
-def useless_function():
-    """ 
-    A function that returns some text
-    """
-    return("some text")
+import textblob as tb
+
+# tag input text with part-of-speech tags
+def tag_pos(text):
+    blob = tb.TextBlob(text)
+    pos = blob.tags
+
+    return pos
+
+# calculate sentiment polarity score of input text [-1, 1]
+def calc_sentiment_score(text):
+    blob = tb.TextBlob(text)
+    sent = blob.sentiment.polarity
+
+    return sent
+
+# autocorrect any spelling mistakes in input text
+def autocorrect_text(text):
+    blob = tb.TextBlob(text)
+    new_text = blob.correct()
+
+    return new_text.raw
+
+# translate input text to the given language
+def translate_text(text, to_lang):
+    blob = tb.TextBlob(text)
+    translated = blob.translate(to=to_lang)
+
+    return translated.raw
